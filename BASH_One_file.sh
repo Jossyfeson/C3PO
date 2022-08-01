f69b0c7e23e5fef7a7cf162adc8914daf307ad86
Jossyfeson <109754736+Jossyfeson@users.noreply.github.com>
2022-08-01 18:03:00 +0100

    Update BASH_One_file.sh

--- BASH_One_file.sh
+++ BASH_One_file.sh
@@ -1,4 +1,14 @@
 #! /bin/sh
+#My Code for names
+echo "What is your first name? "
+read firstname
+echo "Hello $firstname"
+echo "What is your last name? "
+read lastname
+echo "Hello $firstname $lastname "
+#Version were the names are printed on different lines
+echo $firstname
+echo $lastname
 # BASH One story
 mkdir Joseph/
 mkdir biocomputing && cd biocomputing
